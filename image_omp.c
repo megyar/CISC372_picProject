a829e6370262042330a517001ed3099bedd2e441
Meghana Yarlagadda <megyar@ubuntu-2004.eecis.udel.edu>
2024-01-07 00:31:49 -0500

    fixed makefile for image_omp

--- image_omp.c
+++ image_omp.c
@@ -60,8 +60,8 @@ uint8_t getPixelValue(Image* srcImage,int x,int y,int bit,Matrix algorithm){
 void convolute(Image* srcImage,Image* destImage,Matrix algorithm){
     int row,pix,bit,span;
     span=srcImage->bpp*srcImage->bpp;
-
-#pragma omp for
+    
+    #pragma omp for
     for (row=0;row<srcImage->height;row++){
         for (pix=0;pix<srcImage->width;pix++){
             for (bit=0;bit<srcImage->bpp;bit++){
@@ -116,7 +116,7 @@ int main(int argc,char** argv){
     destImage.width=srcImage.width;
     destImage.data=malloc(sizeof(uint8_t)*destImage.width*destImage.bpp*destImage.height);
     
-#pragma omp parallel
+    #pragma omp parallel
     convolute(&srcImage,&destImage,algorithms[type]);
     
     stbi_write_png("output.png",destImage.width,destImage.height,destImage.bpp,destImage.data,destImage.bpp*destImage.width);
--- makefile
+++ makefile
@@ -6,7 +6,7 @@ pthread: pthread.c pthread.h
 	gcc -g pthread.c -o pthread -lm -lpthread
 
 image_omp: image_omp.c image.h
-	gcc -c image_omp.c -o image_omp -lm -fopenmp
+	gcc -g image_omp.c -o image_omp -lm -fopenmp
 
 clean:
 	rm -f image output.png
